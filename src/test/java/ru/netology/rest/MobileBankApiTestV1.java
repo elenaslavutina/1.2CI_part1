f30105928724803a1c722c7dee7522198f000a5a
elenaslavutina <elena.slavutina2013@gmail.com>
2020-05-31 22:40:17 +0300

    add condition about "Текущий зарплатный счёт" in schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -3,6 +3,8 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
 
 class MobileBankApiTestV1 {
     @Test
@@ -16,6 +18,9 @@ class MobileBankApiTestV1 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(200);
+          .statusCode(200)
+          .body("[2].name", equalTo("Текущий зарплатный счёт"))
+        ;
+
     }
 }
